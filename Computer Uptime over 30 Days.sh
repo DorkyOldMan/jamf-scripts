6ff034afe3e615ed7f59a3b167e082a07219b3aa
Micah Coyle <159976548+DorkyOldMan@users.noreply.github.com>
2025-03-06 09:30:25 -0500

    Script to check for Computer Uptime Over 30 Days

---
+++ Computer Uptime over 30 Days.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+# Get system uptime in seconds
+uptime_seconds=$(sysctl -n kern.boottime | awk -F'[ ,]' '{print $4}')
+
+# Get current time in seconds since epoch
+current_time=$(date +%s)
+
+# Calculate the time difference (current time - uptime start time)
+time_diff=$((current_time - uptime_seconds))
+
+# Define 30 days in seconds
+thirty_days_seconds=$((30 * 24 * 60 * 60))
+
+# Check if the system has been online for more than 30 days
+if [ $time_diff -ge $thirty_days_seconds ]; then
+	result="Yes"
+else
+	result="No"
+fi
+
+# Output the result for the Extension Attribute
+echo "<result>$result</result>"
